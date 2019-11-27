0537aa5d27054705ebb41fe3761b426cd2c9f3bb
zyandeep <zyandeep000@gmail.com>
2019-11-27 09:42:41 +0530

    TM crash coures

---
+++ python_sandbox.py
@@ -0,0 +1,26 @@
+"""
+This is
+A multiline comment
+"""
+# x, y, name, is_alive = (2, 3.44, "bob", False)
+# print(x ** 5, y % 2, is_alive, sep=" : ", end="\n")
+# print(type({}), None == False)
+# print(name.swapcase())
+# name[1] = "h"
+# print(name[0])
+# print([3, "bob", True].pop(1))
+# my_list = [3, 5, 1, 4]
+# my_list.sort(reverse=True)
+# print(my_list)
+# my_tuple = ("bob", 12, True, 12)
+# my_tuple[1] = 99
+# my_list = [1, 2, 3, 4]
+# del my_list[1]
+# print(my_tuple, type(my_tuple), my_tuple[2])
+# my_dict = {
+#     "name": "bob",
+#     "age": 23
+# }
+# del my_dict["age"]
+# print(my_dict)
+
