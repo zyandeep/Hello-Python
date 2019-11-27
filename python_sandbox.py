21fdde9abe50b833e8896ab084485c48049e92af
zyandeep <zyandeep000@gmail.com>
2019-11-27 20:14:20 +0530

    python as bash script

--- python_sandbox.py
+++ python_sandbox.py
@@ -17,10 +17,57 @@ A multiline comment
 # my_list = [1, 2, 3, 4]
 # del my_list[1]
 # print(my_tuple, type(my_tuple), my_tuple[2])
+# print(my_dict)
+# my_set = {"bob", "mira", "ram", "john"}
+# print(my_set.remove("bob"))
+# del my_set[]
+# print(2 in my_set)
+# for item in my_set:
+#     print(item)
 # my_dict = {
 #     "name": "bob",
 #     "age": 23
 # }
-# del my_dict["age"]
-# print(my_dict)
+# my_dict["phone"] = "555-555-555"
+# for key in my_dict:
+#     print(my_dict[key], type(key))
+# print(len(my_dict))
+# print(my_dict.items())
+# del(my_dict["age"])
+# print(my_dict.get("age"))
+# my_func = lambda num1, num2: num1 ** num2
+# print(my_func(2, 4))
+# x, y = [4, 5, 8, 9]
+# print(x, y)
+# def swap(a, b):
+#     t = a
+#     a = b
+#     b = t
+#     return (a, b)
+# a = 50
+# b = 17
+# print(f"BEFORE: a = {a}, b = {b}")
+# a, b= swap(a, b)
+# print(f"AFTER: a = {a}, b = {b}")
+# def test(a, b):
+#     return a + b
+# def test(name):
+#     return f"Hello, {name}"
+# print(test("bob"))
+# print(test(2, 6))
+# l1 = lambda x: x + 3
+# l3 = lambda x : l1(l1(x))
+# print(l3(7))
+# r = range(1, 10, 1)
+# print(r)
 
+# class User:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+    
+#     def greeting(self):
+#         return f"Hello, {self.name}. You are {self.age} years old."
+
+# bob = User("Bob", 23)
+# print(bob.greeting())
